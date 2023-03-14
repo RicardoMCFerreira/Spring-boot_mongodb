910f034dc46483aa690382494a68395cd1309b8c
RicardoMCFerreira <manuelriky@hotmail.com>
2023-03-14 00:05:00 +0000

    User insertion with POST

--- src/main/java/com/example/workshoopmongo/services/UserService.java
+++ src/main/java/com/example/workshoopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshoopmongo.domain.User;
+import com.example.workshoopmongo.dto.UserDTO;
 import com.example.workshoopmongo.repository.UserRepository;
 import com.example.workshoopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
+	}
 }
