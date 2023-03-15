b3432c0c1be7de5bc5e2f837354e3828a3ffdfba
RicardoMCFerreira <manuelriky@hotmail.com>
2023-03-15 03:15:35 +0000

    Simple query with @Query

--- src/main/java/com/example/workshoopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshoopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.workshoopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshoopmongo.domain.Post;
@@ -12,4 +13,9 @@ public interface PostRepository extends MongoRepository<Post, String>{
 
 	//https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	//https://docs.mongodb.com/manual/reference/operator/query/regex/
+	//?0 --> informa que é o primeiro parametro da função
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String test);
 }
